a5d17495526230c6dfd940ab77052fc6d9de9b4c
jun218391 <gongzezhun@gmail.com>
2023-02-18 06:57:22 +0000

    [add]adminログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  def after_sign_in_path_for(resource)
-    customers_my_page_path
-  end
-  
-  def after_sign_out_path_for(resource)
-    about_path
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:fast_name,:kana_last_name,:kana_fast_name,:postal_code,:address,:telephone_number])
-  end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,21 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    customers_my_page_path 
+  end
+  
+  def after_sign_out_path_for(resource)
+    about_path 
+  end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:fast_name,:kana_last_name,:kana_fast_name,:postal_code,:address,:telephone_number])
+  end
   # GET /resource/sign_in
   # def new
   #   super
