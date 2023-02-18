ac189d11185929bbe0e94d9d88dcf1d74a437bbf
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-02-18 02:22:37 +0000

    add-customer-page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, :fast_name,:email,:address, presence: true #空白でない
+  validates :kana_last_name, :kana_fast_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } #カタカナのみ
+  validates :password, length: { minimum: 6 } #6文字以上？
+  validates :telephone_number, uniqueness: true #被りなし、一意性
+  validates :postal_code, length: { is: 7 } #7桁制限
+
 end
