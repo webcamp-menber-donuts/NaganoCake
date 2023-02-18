7a4e9dd6369c8312ca50840aaeeff6ed3eec168d
jun218391 <gongzezhun@gmail.com>
2023-02-18 08:23:20 +0000

    [update]adminログイン機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,20 +2,12 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  
   def after_sign_in_path_for(resource)
-    
+    "/admin"
   end
   
   def after_sign_out_path_for(resource)
-    "/" 
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:fast_name,:kana_last_name,:kana_fast_name,:postal_code,:address,:telephone_number])
+    "/admin/sign_in" 
   end
   # GET /resource/sign_in
   # def new
