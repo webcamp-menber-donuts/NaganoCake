56e9374a9f6eda9db540c0b529dbcc5cf60f0279
kaitoppo <122006842+kaitoppo@users.noreply.github.com>
2023-02-19 09:18:31 +0000

    [製作中]shopping_address

--- app/controllers/public/shopping_addresses_controller.rb
+++ app/controllers/public/shopping_addresses_controller.rb
@@ -1,7 +1,15 @@
 class Public::ShoppingAddressesController < ApplicationController
   def index
+    @shopping_addresses = ShoppingAddress.all
+    @shopping_address = ShoppingAddress.new
   end
 
   def edit
+  end
+
+  private
+
+  def shopping_address_params
+    params.require(:shopping_address).permit(:name, :postal_code, :address)
   end
 end
