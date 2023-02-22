bfc1d7b0066c47891f126e66df5ca120a8373c3a
jun218391 <gongzezhun@gmail.com>
2023-02-22 02:43:56 +0000

    [update]商品一覧ページ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < ApplicationController
   def show
     # @order = Order.find(params[:id])
+    # @order_details.id = @order.id
+    # @product.id = @order.product_id
+    # @customer.id = @order.customer_id 
   end
 end
