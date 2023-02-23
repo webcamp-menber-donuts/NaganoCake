accae63a37099f45d3b454bd0d5d0618801c35a8
jun218391 <gongzezhun@gmail.com>
2023-02-23 08:52:45 +0000

    [add]管理者ページ　ステータスの更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
+    def update
+        @order_detail = OrderDetail.find(params[:id])
+        #製作ステータスの変更
+        if @order_detail.update(order_detail_params)
+          flash[:notice] = "製作ステータスを更新しました"
+          redirect_to request.referer
+        else
+          render template: "orders/show"
+        end
+    end
+    
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:product_status)
+  end
 end
