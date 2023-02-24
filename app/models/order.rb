7aa55423ac2881ce1bde8904f00ff13813fdfb03
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-02-24 06:08:51 +0000

    [update]validates

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
   enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
   
   validates :name, :address, :payment_method, :order_status, presence: true
-  validates :postal_code, presence: true, format: {with: /\A\d{7}\z/ } #郵便番号（ハイフンなし7桁）
+  validates :postal_code, length: { is: 7 } #7桁制限
   validates :postage, :total_payment, numericality: true #数値のみ
  
 end
